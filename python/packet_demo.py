327dddd6933fcbe6687676c54dfec3faa87e8dbb
ptracton <ptracton@gmail.com>
2014-10-22 16:29:48 -0700

    Starting up put structure to python comments

--- python/packet_demo.py
+++ python/packet_demo.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python3
 
+'''
+Application Name: packet_demo
+
+Required Libraries:
+serial, QT4
+'''
+
+
+__author__ = "Philip Tracton"
+__copyright__ = "Copyright 2014, Philip Tracton"
+__credits__ = ["Philip Tracton"]
+__license__ = "GPL"
+__version__ = "2.0"
+__maintainer__ = "Philip Tracton"
+__email__ = "ptracton@gmail.com"
+__status__ = "Experimental"
+
 
 if __name__ == "__main__":
+    '''
+    Program Entry Point
+    '''
     print("Packet Demo")
